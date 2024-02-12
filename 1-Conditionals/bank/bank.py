6f8ad90a298dfe2d91ce06f22937f72d880c0856
Roman <124547257+RomanLac@users.noreply.github.com>
2024-02-12 00:48:49 +0000

    Upload completed bank.py

---
+++ 1-Conditionals/bank/bank.py
@@ -0,0 +1,12 @@
+# hello, starts with h, other
+
+greet = input("hi say something: ")
+
+if greet.startswith("Hello"):
+    print("$0")
+
+elif greet.startswith("H"):
+    print("$20")
+
+else:
+    print("$100")
