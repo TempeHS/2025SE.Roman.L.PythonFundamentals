bbc55576eee92f58b4225f5d8cfed6f0d5c0055e
Roman <124547257+RomanLac@users.noreply.github.com>
2024-02-26 04:40:15 +0000

    Upload completed fuel.py

--- 3-Exceptions/fuel/fuel.py
+++ 3-Exceptions/fuel/fuel.py
@@ -3,21 +3,40 @@
 
 def main():
     x = get_fuel()
-    print(f"fuel is {x}")
+
+    if 0 >= x >= 1:
+        print("E")
+
+    elif 99 >= x >= 100:
+        print("F")
+
+    elif x > 100:
+        print("try again")
+        main()
+
+    elif x < 0:
+        print("try again")
+        main()
+
+    else:
+        print(f"{x}%")
 
 
 def get_fuel():
-    try:
-        fuel = int(input("fuel in fraction form x/y: "))
-        x, y = fuel.split("/")
+    while True:
+        try:
+            fuel = input("fuel in fraction form x/y: ")
+            x, y = map(int, fuel.split("/"))
 
-        x = int(x)
-        y = int(y)
+            x = int(x)
+            y = int(y)
 
-        fuel = (x / y) * 100
-        return int(fuel)
-    except (ValueError, ZeroDivisionError):
-        print("fuel is not an integer")
+            fuel_perc = (x / y) * 100
+            roundedfuel = round(fuel_perc)
+
+            return int(roundedfuel)
+        except (ValueError, ZeroDivisionError):
+            print("fuel is not an integer")
 
 
 if __name__ == "__main__":
