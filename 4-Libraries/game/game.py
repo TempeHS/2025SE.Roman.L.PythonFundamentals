3eeac75d85497b11e2661f7ff88223ea5103a91e
Roman <124547257+RomanLac@users.noreply.github.com>
2024-03-11 05:40:07 +0000

    Upload completed game.py

--- 4-Libraries/game/game.py
+++ 4-Libraries/game/game.py
@@ -2,15 +2,49 @@ import random
 
 
 def main():
+    count = 0
+    ranger = 0
+
     while True:
         try:
-            prompt = input("Level: ")
+            if count == 0:
+                n = input("Level: ")
+                thisisanint = int(n)
 
-            if int(prompt) < 0:
-                main()
+                if n.isalnum():
+                    count += 1
+
+                else:
+                    count = 0
+
+            if count == 1:
+                if ranger == 0:
+                    ranger = random.randint(1, thisisanint)
+
+                else:
+                    e = input("Guess: ")
+                    guess = int(e)
+
+                    if e.isalnum():
+                        count += 1
+
+                    else:
+                        count = 1
+
+            # guess for int
+            if count == 2:
+                if ranger == guess:
+                    print("Just right!")
+                    break
+                elif ranger > guess:
+                    print("Too small!")
+                    break
+                else:
+                    print("Too large!")
+                    break
 
         except ValueError:
-            print("a")
+            count == count
 
 
 if __name__ == "__main__":
