e7442ec519fb26c13470839a1067f23461fced69
Roman <124547257+RomanLac@users.noreply.github.com>
2024-03-16 07:09:32 +0000

    Upload completed test_twttr.py

--- 5-UnitTests/test_twttr/test_twttr.py
+++ 5-UnitTests/test_twttr/test_twttr.py
@@ -4,5 +4,5 @@ from twttr import shorten
 def test_shorten():
     assert shorten("test") == "tst"
     assert shorten("hihi") == "hh"
-    assert shorten("hi hello") == "h hll"
+    assert shorten("hi Hello") == "h Hll"
     assert shorten("aeiou") == ""
