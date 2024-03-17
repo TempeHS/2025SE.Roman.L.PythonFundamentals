d86c847624e84f52f29fdba41356bbe633d17562
Roman <124547257+RomanLac@users.noreply.github.com>
2024-03-17 03:58:59 +0000

    Upload completed lines.py

---
+++ 6-FileIO/lines/check.py
@@ -0,0 +1,13 @@
+# this is a comment
+
+
+def main():
+    print("hello world")
+
+    print("hello world")
+    print("hello world")
+    print("hello world")
+
+
+if __name__ == "__main__":
+    main()
---
+++ 6-FileIO/lines/lines.py
@@ -0,0 +1,39 @@
+import sys
+
+
+def main():
+    try:
+        if sys.argv[1].endswith(".py") and len(sys.argv) == 2:
+            count = 0
+            try:
+                with open(sys.argv[1]) as file:
+                    for line in file:
+                        line = line.lstrip()
+                        # line strip considered false if line is empty
+                        if not line.startswith("#") and line.strip():
+                            count += 1
+                print(f"{count} lines of code.")
+
+            except FileNotFoundError:
+                print("File does not exist")
+                sys.exit
+
+        elif len(sys.argv) > 2:
+            print("Too many command-line arguments")
+            sys.exit
+
+        else:
+            sys.exit
+
+    except IndexError:
+        if ".py" not in sys.argv[1]:
+            print("Not a Python file")
+            sys.exit
+
+        else:
+            print("Too few command-line arguments")
+            sys.exit
+
+
+if __name__ == "__main__":
+    main()
